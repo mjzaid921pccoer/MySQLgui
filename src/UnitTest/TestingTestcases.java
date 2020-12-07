3b1a9bf0520c4a9ef40f83c984760f7bead23527
sushantsaid <61629830+sushantsaid@users.noreply.github.com>
2020-12-07 18:24:25 +0530

    Update TestingTestcases.java
    Added testcase for deleteRecord()

--- src/UnitTest/TestingTestcases.java
+++ src/UnitTest/TestingTestcases.java
@@ -1,24 +1,26 @@
 package UnitTest;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-import java.sql.SQLException;
-import java.util.ArrayList;
-
+//import static org.junit.Assert.assertTrue;
+//import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.Assert;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
-
+import org.junit.jupiter.api.TestMethodOrder;
+import java.util.*;
 import DB.DBHelper;
 
-class TestingTestcases {
+@TestMethodOrder(OrderAnnotation.class)
+public class Testing {
 
+	public int i = 0;
 	@Test
+	@Order(1)
 	public void testGetData() {
 		ArrayList data = null;
 		try {
 			DBHelper db = new DBHelper();
 			data = db.getData();
-		} catch (SQLException e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		Assert.assertTrue(data instanceof ArrayList);
@@ -26,29 +28,36 @@ class TestingTestcases {
 	
 	
 	@Test
+	@Order(2)
 	public void testInsertData1() {
+		boolean f = false;
 		try {
 			DBHelper db = new DBHelper();
 			if(db.status)
-			Assert.assertTrue(db.insertData(3, "Akash", "FE", "9876543211", "Pimpri"));
-		} catch (SQLException e) {
+				f = db.insertData(3, "Akash", "FE", "9876543211", "Pimpri");
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		Assert.assertTrue(f);
 	}
 	
 	@Test
+	@Order(3)
 	public void testInsertData2() {
+		boolean f = false;
 		try {
 			DBHelper db = new DBHelper();
 			if(db.status)
-			Assert.assertFalse((db.insertData(3, "Akash", "FE", "9876543211", "Pimpri")));
-		} catch (SQLException e) {
+				f = db.insertData(3, "Akash", "FE", "9876543211", "Pimpri");
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		Assert.assertFalse(f);
 	}
 	
 	@Test
-	public void testDeleteRecord() {
+	@Order(4)
+	public void testDeleteRecord1() {
 		boolean f = false;
 		try {
 			DBHelper db = new DBHelper();
@@ -59,5 +68,20 @@ class TestingTestcases {
 		}
 		Assert.assertTrue(f);
 	}
-
+	
+	@Test
+	@Order(5)
+	public void testDeleteRecord2() {
+		boolean f = false;
+		try {
+			DBHelper db = new DBHelper();
+			if(db.status)
+				f = db.deleteRecord(4);
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		Assert.assertFalse(f);
+	}
+	
 }
+
